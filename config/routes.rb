08ce5583107e54d52bdf78f554e273f58c30b6d3
Yuto <m28m28nem17@gmail.com>
2020-06-13 15:41:42 +0000

    Added Products controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  
   devise_for :admins
   devise_for :users
-
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root to: "products#index"
+
 end
