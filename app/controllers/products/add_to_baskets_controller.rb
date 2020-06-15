1e8ce9c89a77104c43d81fb708bc09d9ae4311f0
Yuto <m28m28nem17@gmail.com>
2020-06-15 18:09:28 +0000

    Added controller delete_in_baskets

--- app/controllers/products/add_to_baskets_controller.rb
+++ app/controllers/products/add_to_baskets_controller.rb
@@ -1,8 +1,9 @@
 class Products::AddToBasketsController < Products::ApplicationController
-
   def create
   basket = current_user.prepare_basket
   product = Product.find(params[:product_id])
   baskt.basket_productscreate!(product_id: product.id)
+
+  redirect_to basket_path
   end
 end
