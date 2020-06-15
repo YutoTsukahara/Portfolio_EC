8da94de710300bcd02c0131f3d0f3f7f79341fb6
Yuto <m28m28nem17@gmail.com>
2020-06-15 15:58:33 +0000

    Added model BasketProduct

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_150832) do
+ActiveRecord::Schema.define(version: 2020_06_15_154355) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2020_06_15_150832) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "basket_products", force: :cascade do |t|
+    t.integer "basket_id", null: false
+    t.integer "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["basket_id"], name: "index_basket_products_on_basket_id"
+    t.index ["product_id"], name: "index_basket_products_on_product_id"
   end
 
   create_table "baskets", force: :cascade do |t|
@@ -60,6 +69,8 @@ ActiveRecord::Schema.define(version: 2020_06_15_150832) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "basket_products", "baskets"
+  add_foreign_key "basket_products", "products"
   add_foreign_key "baskets", "users"
   add_foreign_key "products", "admins"
 end
