17423829b2ef87afd63552433beb6191b86b70f1
Yuto <m28m28nem17@gmail.com>
2020-06-22 00:14:30 +0000

    Added login page of admin

---
+++ app/controllers/admins/products_controller.rb
@@ -0,0 +1,21 @@
+class Admins::ProductsController < Admins::ApplicationController
+  
+  def new 
+    @product = current_admin.products.build
+  end
+
+  def create 
+    @product = Product.current_admin.products.build(product_params)
+    if @product.save
+      redirect_to root_path, notice: '商品を追加しました。'
+    else
+      flash[:error] = '追加に失敗しました。'
+      render :new
+    end
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :description, :price, :image)
+    end
+end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,28 +3,10 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
-  def new 
-    @product = Product.new
-  end
-
-  def create 
-    @product = Product.new(product_params)
-    if @product.save
-      redirect_to root_path, notice: '商品を追加しました。'
-    else
-      flash[:error] = '追加に失敗しました。'
-      render :new
-    end
-  end
-
     def show 
       @product = Product.find(params[:id])
     end
   
-    private
-    def product_params
-      params.require(:product).permit(:name, :description, :price, :image)
-    end
     
   end
 
